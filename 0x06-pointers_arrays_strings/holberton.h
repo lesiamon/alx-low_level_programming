74dd63ab5bdf21f8f2dd46a0f8f80ac7dcf8bd3a
lesiamon <kiolalesiamon@gmail.com>
2023-03-01 09:06:05 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,22 +1,14 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#include "holberton.h"
+#include <unistd.h>
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month,int day);
-void print_remaining_days(int month,int day,int year);
-void print_to_98(int n);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-int _putchar(char c);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-
-#endif
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
