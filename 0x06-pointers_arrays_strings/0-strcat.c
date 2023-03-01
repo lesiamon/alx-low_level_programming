add1dfaad9bd842130d64b5dc7c7b8fd72809971
lesiamon <kiolalesiamon@gmail.com>
2023-03-01 09:27:55 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 
 /**
- * memset - Fills the first n bytes of the memory area
- *          pointed to by @s with the constant byte @c.
- * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
- * @n: The number of bytes to be filled.
+ * _strncat - Concatenates two strings using at most
+ *            an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
  *
- * Return: A pointer to the filled memory area @s.
+ * Return: A pointer to the resulting string dest.
  */
-void *_memset(void *s, int c, size_t n)
+char *_strncat(char *dest, char *src, int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+	int index = 0, dest_len = 0;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
