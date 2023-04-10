3350b49f2f95268c58a15a3d37509c7e09289a7e
lesiamon <koilalesiamon@gmail.com>
2023-04-09 23:26:06 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,13 @@
+#include "main.h"
+unsigned int flip_bits(unsigned long int p, unsigned long int k)
+{
+	unsigned long int xor = n ^ m, bits = 0;
+
+	while (xor > 0)
+	{
+		bits += (xor & 1);
+		xor >>= 1;
+	}
+
+	return (bits);
+}
